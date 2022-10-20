dbf2ca6933c29b35a0169d1c3791d4ede64eec5f
PavelGunko <g9636406069@yandex.ru>
2022-10-21 01:40:41 +0300

    Cropped Middle

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales= {10, 20, 30};
+        int[] sales = {10, 20, 30, 40, 50, 60};
         SalesManager manager = new SalesManager(sales);
-        System.out.println(manager.max());
+        System.out.println("Максимальная сумма " + manager.max());
+        System.out.println("Минимальная сумма " + manager.min());
+        System.out.println("Средняя сумма " + manager.croppedMiddle());
     }
 }
